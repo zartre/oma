e95e4f46d2f13848d964c5848b38cfd220a88dc7
Nathan Yiangsuppanontr <git@zartre.com>
2024-02-21 21:50:12 +0700

    refactor: style CounterItem

--- src/lib/CounterItem.svelte
+++ src/lib/CounterItem.svelte
@@ -7,13 +7,27 @@
 </script>
 
 <div class="counter-item">
-	<h2>{title}</h2>
-	<span>{daysSince}</span>
+	<h4>{title}</h4>
+	<p>{daysSince}</p>
 </div>
 
 <style>
 	.counter-item {
-		margin-bottom: 1rem;
-		padding: 0 1em;
+		background-color: var(--color-surface);
+		border-radius: var(--border-radius);
+		margin-bottom: 0.5rem;
+		padding: 1rem;
+	}
+	.counter-item h4 {
+		margin: 0 0 0.5rem;
+	}
+	.counter-item p {
+		font-size: 1.8rem;
+		line-height: 1em;
+		margin: 0;
+		text-align: right;
+	}
+	.counter-item:last-of-type {
+		margin-bottom: 6em;
 	}
 </style>
