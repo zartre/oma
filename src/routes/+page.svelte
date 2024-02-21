2cf5dab4e3e5ae5796f3ae764bd80b977b68c56f
Nathan Yiangsuppanontr <git@zartre.com>
2024-02-21 21:51:06 +0700

    refactor: style Add btn in home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,14 +10,31 @@
 	<CounterItem title="See Oum" sinceDate={new Date(2024, 0, 14)} />
 </main>
 <footer>
-	<button>Add</button>
+	<a class="button" href="/create">Add</a>
 </footer>
 
 <style>
 	header {
+		padding: 30px 1rem 0;
+	}
+	main {
 		padding: 0 1rem;
 	}
 	footer {
+		backdrop-filter: blur(15px);
+		background-color: rgba(255, 255, 255, 0.2);
+		bottom: 0;
+		left: 0;
 		padding: 1rem;
+		position: fixed;
+		right: 0;
+		text-align: center;
+		width: 100%;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		footer {
+			background-color: rgba(0, 0, 0, 0.2);
+		}
 	}
 </style>
