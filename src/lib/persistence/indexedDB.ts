c3fadcd2a67ff8a8a4b3bed30cb7ac2a1d3d67c9
Nathan Yiangsuppanontr <git@zartre.com>
2024-06-18 23:00:31 +0700

    refactor: make IDB initialisation async

--- src/lib/persistence/indexedDB.ts
+++ src/lib/persistence/indexedDB.ts
@@ -10,34 +10,47 @@ export class IDB implements Persistence {
 	private static instance: IDB
 	private db!: IDBDatabase
 
-	private constructor() {
-		// Check for window object so that SvelteKit can render pages on server side.
-		if (typeof window == 'undefined') {
-			console.warn('indexedDB.ts: window is undefined. This should happen only in SSR.')
-			return
-		}
-
-		const req = window.indexedDB.open(PUBLIC_DB_NAME, 1)
-		req.onerror = (event) => {
-			console.error('Cannot open a DB connection:', (<IDBOpenDBRequest>event.target).error?.message)
-			throw (<IDBOpenDBRequest>event.target).error
-		}
-		req.onsuccess = (event) => {
-			this.db = (<IDBOpenDBRequest>event.target).result
-		}
-		req.onupgradeneeded = (event) => {
-			const db = (<IDBOpenDBRequest>event.target).result
-
-			db.createObjectStore(this.TRACKED_EVENT_STORE_NAME, { autoIncrement: true })
-		}
-	}
+	private constructor() {}
 
 	static getInstance(): IDB {
 		if (!this.instance) this.instance = new IDB()
 		return this.instance
 	}
 
+	private initDB() {
+		return new Promise<void>((resolve, reject) => {
+			// Check for window object so that SvelteKit can render pages on server side.
+			if (typeof window == 'undefined') {
+				console.warn('indexedDB.ts: window is undefined. This should happen only in SSR.')
+				resolve()
+			}
+
+			const req = window.indexedDB.open(PUBLIC_DB_NAME, 1)
+
+			req.onerror = (event) => {
+				console.error(
+					'Cannot open a DB connection:',
+					(<IDBOpenDBRequest>event.target).error?.message
+				)
+				reject((<IDBOpenDBRequest>event.target).error)
+			}
+
+			req.onsuccess = (event) => {
+				this.db = (<IDBOpenDBRequest>event.target).result
+				resolve()
+			}
+
+			req.onupgradeneeded = (event) => {
+				const db = (<IDBOpenDBRequest>event.target).result
+
+				db.createObjectStore(this.TRACKED_EVENT_STORE_NAME, { autoIncrement: true })
+			}
+		})
+	}
+
 	async createEvent(e: TrackedEvent): Promise<void> {
+		if (!this.db) await this.initDB()
+
 		const txn = this.db.transaction(this.TRACKED_EVENT_STORE_NAME, 'readwrite')
 
 		return new Promise((resolve, reject) => {
