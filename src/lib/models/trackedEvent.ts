5574ee16f132df8d4d4a2dd15c805b1bffc3876c
Nathan Yiangsuppanontr <git@zartre.com>
2024-06-25 23:02:18 +0700

    refactor: generate uuid in TrackedEvent

--- src/lib/models/trackedEvent.ts
+++ src/lib/models/trackedEvent.ts
@@ -1,14 +1,27 @@
+import { v4 as uuid } from '@lukeed/uuid'
+
 /**
  * TrackedEvent represents an item in the app where
  * a user tracks its last concerned date.
  */
 export class TrackedEvent {
-	id: number = 0
+	id: string = ''
 	title: string
 	date: Date
 
-	constructor(title: string, date: Date) {
+	constructor(title: string, date: Date, opts?: TrackedEventOption) {
 		this.title = title
 		this.date = date
+
+		if (!opts?.id) this.generateID()
 	}
+
+	generateID() {
+		this.id = uuid()
+	}
+}
+
+interface TrackedEventOption {
+	// If not given, a new ID will be assigned to the TrackedEvent.
+	id?: string
 }
