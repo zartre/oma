dc9e0df500eebb9e9d62631f084e19fee509d9dd
Nathan Yiangsuppanontr <git@zartre.com>
2024-07-02 20:00:26 +0700

    style: format code

--- src/lib/AddEventModal.svelte
+++ src/lib/AddEventModal.svelte
@@ -1,73 +1,74 @@
 <script lang="ts">
-	import { TrackedEvent } from '$lib/models/trackedEvent';
-	import { persistentStore } from '$lib/persistence/persistence';
+	import { TrackedEvent } from '$lib/models/trackedEvent'
+	import { persistentStore } from '$lib/persistence/persistence'
 	import { createEventDispatcher } from 'svelte'
 
-	let eventTitle = '';
-	let eventDate = '';
+	let eventTitle = ''
+	let eventDate = ''
 
-    const dispatch = createEventDispatcher();
+	const dispatch = createEventDispatcher()
 
-    const dispatchSuccess = () => dispatch('success', true)
+	const dispatchSuccess = () => dispatch('success', true)
 
 	const save = async () => {
-		const d = new Date(eventDate);
-		const ev = new TrackedEvent(eventTitle, d);
+		const d = new Date(eventDate)
+		const ev = new TrackedEvent(eventTitle, d)
+		// TODO: Also add to Store
 		try {
-			await persistentStore.createEvent(ev);
-			dispatchSuccess();
+			await persistentStore.createEvent(ev)
+			dispatchSuccess()
 		} catch (err) {
 			alert(`Cannot save: ${err}`)
 		}
-	};
+	}
 </script>
 
 <div class="modal">
-    <header>
-        <button class="text-button" on:click={dispatchSuccess}>Cancel</button>
-        <h1>New</h1>
-    </header>
+	<header>
+		<button class="text-button" on:click={dispatchSuccess}>Cancel</button>
+		<h1>New</h1>
+	</header>
 
-    <main>
-        <form on:submit|preventDefault={save}>
-            <label for="title">Title</label>
-            <input
-                type="text"
-                id="title"
-                autocomplete="off"
-                placeholder="What to call this item?"
-                required
-                bind:value={eventTitle}
-            />
+	<main>
+		<form on:submit|preventDefault={save}>
+			<label for="title">Title</label>
+			<input
+				type="text"
+				id="title"
+				autocomplete="off"
+				placeholder="What to call this item?"
+				required
+				bind:value={eventTitle}
+			/>
 
-            <label for="event-date">Last date</label>
-            <input
-                type="date"
-                id="date"
-                autocomplete="off"
-                placeholder="Pick the last date"
-                required
-                bind:value={eventDate}
-            />
+			<label for="event-date">Last date</label>
+			<input
+				type="date"
+				id="date"
+				autocomplete="off"
+				placeholder="Pick the last date"
+				required
+				bind:value={eventDate}
+			/>
 
-            <div class="form-buttons">
-                <button class="button">Add</button>
-            </div>
-        </form>
-    </main>
+			<div class="form-buttons">
+				<button class="button">Add</button>
+			</div>
+		</form>
+	</main>
 </div>
 
 <style>
-    .modal {
-        background-color: var(--color-bg);
-        overflow: auto;
-        position: fixed;
-        top: 0;
-        left: 0;
-        height: 100%;
-        width: 100%;
-        z-index: 5;
-    }
+	.modal {
+		background-color: var(--color-bg);
+		overflow: auto;
+		position: fixed;
+		top: 0;
+		left: 0;
+		height: 100%;
+		width: 100%;
+		z-index: 5;
+	}
 
 	main {
 		padding: 0 1em;
